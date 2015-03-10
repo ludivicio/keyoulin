e964402450123c6e72af16994b8dce0c95e67abd
lxc <ludivicio@gmail.com>
2015-03-10 11:11:43 +0800

    实现订单处理

--- handle.php
+++ handle.php
@@ -6,36 +6,34 @@
 
 	session_start();
 
-	// 验证码是否正确
-	echo "a" . isset($_POST["code"]);
-
-	$code = $_POST["code"];
-	if($code != $_SESSION["captcha"]) {
-		echo "验证码输入错误";
-		return;
-	} 
-
 	// 验证收货人
 	if($_POST["name"] == "") {
-		echo "请输入收货人姓名";
+		echo json(array('status' => 'failed', 'msg' => '请输入收货人姓名'));
 		return;
 	} 
 
 	// 验证收货地址
 	if($_POST["address"] == "") {
-		echo "请输入收货人地址";
+		echo json(array('status' => 'failed', 'msg' => '请输入收货人地址'));
 		return;
 	}
 
 	// 验证手机号
 	if($_POST["phone"] == "") {
-		echo "请输入手机号";
+		echo json(array('status' => 'failed', 'msg' => '请输入手机号'));
 		return;
 	} elseif(!is_mobile($_POST["phone"])) {
-		echo "手机号不合法，请重新输入";
+		echo json(array('status' => 'failed', 'msg' => '手机号不合法，请重新输入'));
 		return;
 	}
 	
+	// 验证码是否正确
+	$code = $_POST["code"];
+	if($code != $_SESSION["captcha"]) {
+		echo json(array('status' => 'failed', 'msg' => '验证码输入错误'));
+		return;
+	} 
+
 	// 过滤非法字符
 	$name = htmlspecialchars($_POST["name"]);
 	$phone = $_POST["phone"];
@@ -63,6 +61,8 @@
 	
 	/**
 	 * 验证手机号码是否合法
+	 * @param  [type]  $str [description]
+	 * @return boolean      [description]
 	 */
 	function is_mobile($str){    
 		return preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/", $str);   
@@ -70,6 +70,7 @@
 
 	/**
 	 * 获取访问者的IP
+	 * @return [type] [description]
 	 */
 	function get_ip(){
 		
@@ -84,6 +85,18 @@
 		}
 
 		return $cip;
+	}
+
+	/**
+	 * 返回json数据，并处理中文乱码问题
+	 * @param  [type] $array [description]
+	 * @return [type]        [description]
+	 */
+	function json($array) {
+	    foreach ( $array as $key => $value ) {   
+	        $array[$key] = urlencode($value);   
+	    }   
+	    return urldecode(json_encode($array));   
 	}
 
 ?>
